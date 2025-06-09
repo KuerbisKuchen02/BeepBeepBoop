56531b098a7b65098f6e796b24159f580b9aa4c0
Josia Menger <josia.menger@gmx.de>
2025-06-09 15:37:11 +0200

    Recording??

--- App.js
+++ App.js
@@ -2,20 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Button, Text, View } from 'react-native';
 import { useAudioPlayer} from 'expo-audio';
 import { encodeMorse, decodeMorse } from './morse_util.js';
+import { useAudioRecorder, AudioModule, RecordingPresets } from 'expo-audio';
 
 import * as Sharing from 'expo-sharing';
 
-
-async function shareWavFile(uri) {
-  const isAvailable = await Sharing.isAvailableAsync();
-  if (isAvailable) {
-    await Sharing.shareAsync(uri);
-  } else {
-    console.log("Sharing nicht verfügbar");
-  }
-}
-
 export default function App() {
+  const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
+  const [isRecording, setIsRecording] = useState(false);
   const [uri, setUri] = useState(null);
   const player = useAudioPlayer(uri);
 
@@ -27,9 +20,30 @@ export default function App() {
     });
   }, []);
 
+  const record = async () => {
+    await audioRecorder.prepareToRecordAsync();
+    audioRecorder.record();
+    setIsRecording(true);
+  };
+
+  const stopRecording = async () => {
+    // The recording will be available on `audioRecorder.uri`.
+    await audioRecorder.stop();
+    setIsRecording(false);
+    setUri(audioRecorder.uri);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const status = await AudioModule.requestRecordingPermissionsAsync();
+      if (!status.granted) {
+        Alert.alert('Permission to access microphone was denied');
+      }
+    })();
+  }, []);
+
   useEffect(() => {
     if (!uri) return;
-    //shareWavFile(uri);
     console.log("Decoding Morse code from URI:", uri);
     decodeMorse(uri).then((text) => {
       console.log("Decoded Morse text:", text);
@@ -42,6 +56,10 @@ export default function App() {
       <Button title="Play Morse" onPress={() => {console.log("Starte Morse code"); player.play();}} disabled={!uri} />
       <Button title="Pause" onPress={player.pause} disabled={!uri} />
       <Button title="Stop" onPress={player.stop} disabled={!uri} />
+      <Button
+        title={isRecording ? 'Stop Recording' : 'Start Recording'}
+        onPress={isRecording ? stopRecording : record}
+      />
     </View>
   );
 }
