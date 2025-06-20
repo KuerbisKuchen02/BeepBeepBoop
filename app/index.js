ec442695ec4e7ca664376ce881edb7bffadc8354
FalkiFox <juwol1423@gmail.com>
2025-06-20 15:35:45 +0200

    feat: Added basic styling, removed buttons

--- app/index.js
+++ app/index.js
@@ -1,13 +1,37 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
     const router = useRouter();
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home Screen</Text>
-            <Button title="Go to Input" onPress={() => router.push('/callsign')} />
-            <Button title="Go to Chat" onPress={() => router.push('/chat')} />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 100 }}>
+            <Text style= { styles.homeText }>Beep Beep Boop</Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center', gap: 20, width: "100%" }}>
+              <TouchableOpacity style={styles.homeButton} onPress={() => router.push('/callsign')}>
+                <Text style={styles.homeButtonText}>My callsign</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.homeButton} onPress={() => router.push('/chat')}>
+                <Text style={styles.homeButtonText}>Chat</Text>
+              </TouchableOpacity>
+            </View>
+            
         </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homeText: {
+    fontSize: 30.0
+  },
+  homeButton: {
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+    padding: 15,
+    width: "50%"
+  },
+  homeButtonText: {
+    fontSize: 20.0,
+    textAlign: "center"
+  }
+})
